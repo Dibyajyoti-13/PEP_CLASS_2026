94de0e74d45cbda230a6fc0452f419e1c3c73162
Dibyajyoti-13 <dibyajyotisarkar05@gmail.com>
2026-02-16 09:19:16 +0530

    13022026

---
+++ PEP/Tree.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+/*
+    Tree -> it is a non-linear, hierarchical data structure consisting of nodes connected by edges
+    Binary tree -> a tree is a binary tree when every node has at most two children
+
+    Terminologies -
+
+    1. Leaf -> a node with no children
+    2. internal node -> a node which is not a leaf
+    3. Siblings -> the children of same parent
+    4. Length Path -> length of path is 1 less then the number of nodes on the path
+    5. Ancestors and Descendents -> if there is a path from node A to Node B, then A is called an ancestor of B and B is called a descendent of A
+    6 -> subtree -> any node of a tree, with all of its descendents is a subtree
+    7-> Level -> the level of the node referes to its distance from the root, the root of the tree has generally level 0
+    8 -> height -> the maximum level in a tree determines its height, The height of a node in a tree is the length of the longest path from that node to a leaf
+    9-> Depth -> the depth of a node is the number of nodes along the path from the root to that node
+
+    Properties of BT
+        1-> if h = height of a binary tree, then
+            a -> maximum number of leaves = 2^h
+            b-> maximum number of nodes = 2^(h+1) - 1
+        2-> if abinary tree contains m nodes at level l, it contains at mosr 2m nodes at level l + 1
+        3 -> since a binary tree can contain at most 1 node at level 0(root), it can contain at most 2^l nodes at level l
+        4-> the total number of eges in a full binary tree with n nodes is n - 1
+
+
+        Types
+        Full BT -> a tree of height h has all its leaves at level h, means all non leaf nodes of a FBT have two children, and the leaf node have no children
+
+        Complete Binary tree -> all levels are filled except the lest level, the last level should be filled from left to right
+*/
+
+
+class Node{
+    public:
+    int data;
+    Node* right;
+    Node* left;
+
+    Node(int val) : data(val), right(nullptr), left(nullptr) {}
+
+    int countLeafNodes(Node* root){
+        if(root->left == nullptr && root->right == nullptr) return 1;
+
+        if(root->left != nullptr) return countLeafNodes(root->left);
+        if(root->right != nullptr) return countLeafNodes(root->right);
+    }
+};
+
+// void inorder(Node* root){
+//     if(root== NULL) return;
+
+//     inorder(root->left);
+//     cout << root->data << " ";
+//     inorder(root->right);
+
+// }
+
+// void preOrder(Node* root){
+//     if(root== NULL) return;
+
+//     cout << root->data << " ";
+//     preOrder(root->left);
+//     preOrder(root->right);
+
+// }
+
+// void postOrder(Node* root){
+//     if(root== NULL) return;
+
+//     postOrder(root->left);
+//     postOrder(root->right);
+//     cout << root->data << " ";
+
+// }
+
+int countNodes(Node* root){
+    //base case 
+    if(root==NULL) return 0;
+
+    //count nodes in lst
+    int x = countNodes(root->left);
+
+    //count nodes in rst
+    int y = countNodes(root->right);
+
+    return x + y + 1;
+
+}
+
+int countLeaf(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    if(root->left == NULL && root->right == NULL) return 1;
+    int x = countLeaf(root->left);
+    int y = countLeaf(root->right);
+
+    return x + y;
+}
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << countLeaf(root) << endl;
+}
